64a59d236c91b0031f0091f486edc5e168af1d6a
George Boots <ggboots.99@gmail.com>
2020-06-23 08:36:27 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,3 +7,8 @@ function updatePlaylist(playlist, artist, song){
   playlist[artist] = song
   return playlist
 }
+
+function updateFromPlaylist(playlist,artist){
+  delete playlist[artist]
+  return playlist
+}
